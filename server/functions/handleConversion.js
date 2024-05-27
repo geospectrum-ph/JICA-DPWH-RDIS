e2cff74350a2be1e15144372fe002ab72fd4cf03
Geospectrum Marketing Services - IT Department <52265967+geospectrum-ph@users.noreply.github.com>
2024-05-27 17:51:42 +0800

    commit-2024-05-27

--- server/functions/handleConversion.js
+++ server/functions/handleConversion.js
@@ -1,56 +1,38 @@
-// const initGdalJs = require("gdal3.js");
-// const path = require("path");
-
-// initGdalJs().then((Gdal) => {});
-
-// async function convert() {
-//   const Gdal = await initGdalJs();
-//   const dataset = await Gdal.open(path.join(__dirname, "..", "assets/files/sample.geojson"));
-
-//   console.log(dataset);
-
-//   // const file_container = file_array.datasets[0];
-
-//   // console.log(file_container);
-
-//   // const options = [
-//   //   "-f", "GeoJSON",
-//   //   "-t_srs", "EPSG:4326"
-//   // ];
-
-//   // console.log(options);
-
-//   // console.log(file_container.path);
-
-//   // const file_converted = await Gdal.ogr2ogr(file_container, options);
-
-//   // const output = await Gdal.ogr2ogr(result.datasets[0], options);
-//   // const bytes = await Gdal.getFileBytes(output)
-  
-//   // console.log(bytes);
-
-//   Gdal.close(dataset);
-// }
-
 const initGdalJs = require("gdal3.js/node");
+
+const fs = require("fs");
 
 const path = require("path");
 
 async function convert() {
-  await Promise.all([
-    (async() => {
-      const Gdal = await initGdalJs();
-      const dataset = await Gdal.open(path.join(__dirname, "..", "assets/files/sample.kml"));
-      const options = [
-        "-f", "GeoJSON",
-        "-t_srs", "EPSG:4326"
-      ];
+  const gdal = await initGdalJs();
+  
+  const options = [
+    "-f", "GeoJSON",
+    "-t_srs", "EPSG:4326"
+  ];
 
-      const filePath = await Gdal.ogr2ogr(dataset, options);
+  // const options = [ "format", "KML" ];
 
-      // Gdal.close(dataset);
-    })()
-  ]);
+  let source = path.join(__dirname, "..", "assets/files/sample.kml");
+
+  console.log(source);
+
+  let file = await gdal.open(source).then((response) => { return (response); }).catch((error) => { console.log(error); return (null); }); // returns Promise.<TypeDefs.DatasetList>
+
+  console.log(file);
+
+  let info = await gdal.ogr2ogr(file.datasets[0], options, "sample").then((response) => { return (response); }).catch((error) => { console.log(error); return (null); });
+
+  console.log(info);
+
+  let somewhere = path.join(__dirname, "..", "assets/files", info.all[0].local);
+
+  console.log(somewhere);
+
+  // let output = await gdal.open(somewhere).then((response) => { return (response); }).catch((error) => { console.log(error); return (null); }); // returns Promise.<TypeDefs.DatasetList>
+
+  // console.log(output);
 }
 
 module.exports = { convert };
