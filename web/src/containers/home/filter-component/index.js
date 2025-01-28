a3f8a4efcad87a6f8939428f2f27b369257f2b9f
Geospectrum Marketing Services - IT Department <52265967+geospectrum-ph@users.noreply.github.com>
2025-01-28 18:45:47 +0800

    build: 2025-01-28-18-43
    pending items:
    > admin boundary highlight on filter select
    > .xls/.csv format for data export
    > attachment retrieval
    > minor UI/UX revisions (Summary module layout, font and spacing sizes, etc.)

--- web/src/containers/home/filter-component/index.js
+++ web/src/containers/home/filter-component/index.js
@@ -5,11 +5,6 @@ import { MapContext } from "../../../contexts/MapContext";
 
 import "./index.css";
 
-/* Parser for filter levels; To be updated once live data is available. */
-import array_level_01 from "./filter_level_01.json";
-import array_level_02 from "./filter_level_02.json";
-import array_level_03 from "./filter_level_03.json";
-
 export default function FilterComponent () {
   const {
     dataSource,
@@ -28,10 +23,97 @@ export default function FilterComponent () {
   
   const {
     layer_national_road_network,
+    layer_engineering_districts,
+
     layer_road_slopes_and_countermeasures,
 
     recenter_map, close_popup
   } = React.useContext(MapContext);
+
+  const [filterArray, setFilterArray] = React.useState([]);
+
+  React.useEffect(function () {
+    layer_engineering_districts
+      .queryFeatures({
+        where: "1 = 1",
+        returnGeometry: false,
+        outFields: ["*"]
+      })
+      .then(function (response) {
+        if (response && response.features && response.features.length > 0) {
+          for (const feature of response.features) {
+            const buffer_array = filterArray;
+
+            const string_array = /^(.*) ?\((.*)\)$/.exec(feature.attributes.CONG_DIST);
+            const orders_string = /^(.*) DISTRICT$/.exec(string_array[2]);
+            const orders_array = orders_string[1].split(/[\s,&]+/);
+
+            for (const item of orders_array) {
+              const new_entry = string_array[1] + "(" + item + " DISTRICT)";
+
+              const match_array = buffer_array.filter(function (item) {
+                return (item.REGION === feature.attributes.REGION && item.DEO === feature.attributes.DEO && item.CONG_DIST === new_entry);
+              });
+
+              if (match_array.length === 0 && item !== "to") {
+                buffer_array.push({
+                  "REGION": feature.attributes.REGION,
+                  "DEO": feature.attributes.DEO,
+                  "CONG_DIST": new_entry
+                });
+              }
+            }
+
+            setFilterArray(buffer_array);
+          }
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const values = new Map([["I", 1], ["V", 5], ["X", 10]]);
+  
+  function parseRomanToInteger (string) {
+    let result = 0, current, previous = 0;
+
+    for (const char of string.split("").reverse()) {
+      current = values.get(char);
+
+      if (current >= previous) {
+        result += current;
+      }
+      else {
+        result -= current;
+      }
+
+      previous = current;
+    }
+
+    return (result.toString().padStart(2, "0"));
+  }
+
+  function parseOrdinalStringToNumericalString (string) {
+    switch (string) {
+      case "FIRST":  return ("01");
+      case "SECOND": return ("02");
+      case "THIRD": return ("03");
+      case "FOURTH": return ("04");
+      case "FIFTH": return ("05");
+      case "SIXTH": return ("06");
+      case "SEVENTH": return ("07");
+      case "EIGHTH": return ("08");
+      case "NINTH": return ("09");
+      case "TENTH": return ("10");
+      case "ELEVENTH": return ("11");
+      case "TWELFTH": return ("12");
+      case "THIRTEENTH": return ("13");
+      case "FOURTEENTH": return ("14");
+      case "FIFTEENTH": return ("15");
+      default: return (string);
+    }
+  }
 
   function query_features (level, object) {
     setDataArray(null);
@@ -42,14 +124,14 @@ export default function FilterComponent () {
         object ?
           level === 0 ? "1 = 1" :
           level === 1 ? "region_name = '" + object.REGION + "'" :
-          level === 2 ? "region_name = '" + object.REGION + "' AND deo_name = '" + object.DEO + "'" :
-          level === 3 ? "region_name = '" + object.REGION + "' AND deo_name = '" + object.DEO + "' AND district_name = '" + object.CONG_DIST + "'" :
-          level === 4 ? "road_id LIKE '%" + object.query + "%' OR road_name LIKE '%" + object.query + "%' OR section_id LIKE '%" + object.query + "%'" || "1 = 0" :
+          level === 2 ? "deo_name = '" + object.DEO + "'" :
+          level === 3 ? "district_name = '" + object.CONG_DIST + "'" :
+          level === 4 ? "road_id LIKE '%" + object + "%' OR road_name LIKE '%" + object + "%' OR section_id LIKE '%" + object + "%'" || "1 = 0" :
           "1 = 0" :
         level === 0 ? "1 = 1" :
         level === 1 ? "region_name = '" + filterL01Selected + "'" :
-        level === 2 ? "region_name = '" + filterL01Selected + "' AND deo_name = '" + filterL02Selected + "'" :
-        level === 3 ? "region_name = '" + filterL01Selected + "' AND deo_name = '" + filterL02Selected + "' AND district_name = '" + filterL03Selected + "'" :
+        level === 2 ? "deo_name = '" + filterL02Selected + "'" :
+        level === 3 ? "district_name = '" + filterL03Selected + "'" :
         "1 = 0";
 
       dataSource
@@ -90,14 +172,14 @@ export default function FilterComponent () {
         object ?
           level === 0 ? "1 = 1" :
           level === 1 ? "REGION = '" + object.REGION + "'" :
-          level === 2 ? "REGION = '" + object.REGION + "' AND DEO = '" + object.DEO + "'" :
-          level === 3 ? "REGION = '" + object.REGION + "' AND DEO = '" + object.DEO + "' AND CONG_DIST = '" + object.CONG_DIST + "'" :
-          level === 4 ? "ROAD_ID LIKE '%" + object.query + "%' OR ROAD_NAME LIKE '%" + object.query + "%' OR SECTION_ID LIKE '%" + object.query + "%'" || "1 = 0" :
+          level === 2 ? "DEO = '" + object.DEO + "'" :
+          level === 3 ? "CONG_DIST = '" + object.CONG_DIST + "'" :
+          level === 4 ? "ROAD_ID LIKE '%" + object + "%' OR ROAD_NAME LIKE '%" + object + "%' OR SECTION_ID LIKE '%" + object + "%'" || "1 = 0" :
           "1 = 0" :
         level === 0 ? "1 = 1" :
         level === 1 ? "REGION = '" + filterL01Selected + "'" :
-        level === 2 ? "REGION = '" + filterL01Selected + "' AND DEO = '" + filterL02Selected + "'" :
-        level === 3 ? "REGION = '" + filterL01Selected + "' AND DEO = '" + filterL02Selected + "' AND CONG_DIST = '" + filterL03Selected + "'" :
+        level === 2 ? "DEO = '" + filterL02Selected + "'" :
+        level === 3 ? "CONG_DIST = '" + filterL03Selected + "'" :
         "1 = 0";
 
       layer_national_road_network
@@ -200,10 +282,12 @@ export default function FilterComponent () {
     }
   }
   
-  function select_filter (type, object) {
+  function select_filter (type, string) {
     close_popup();
 
     if (type === 1) {
+      const object = filterArray.find(function (item) { return (item.REGION === string); });
+
       setFilterL01Selected(object.REGION);
       setFilterL02Selected(null);
       setFilterL03Selected(null);
@@ -212,7 +296,9 @@ export default function FilterComponent () {
       query_features(1, object);
     }
     if (type === 2) {
-      setFilterL01Selected(object.REGION);
+      const object = filterArray.find(function (item) { return (item.DEO === string); });
+
+      if (!filterL01Selected) setFilterL01Selected(object.REGION);
       setFilterL02Selected(object.DEO);
       setFilterL03Selected(null);
       setFilterL04Selected(null);
@@ -220,8 +306,10 @@ export default function FilterComponent () {
       query_features(2, object);
     }
     if (type === 3) {
-      setFilterL01Selected(object.REGION);
-      setFilterL02Selected(object.DEO);
+      const object = filterArray.find(function (item) { return (item.CONG_DIST === string); });
+
+      if (!filterL01Selected) setFilterL01Selected(object.REGION);
+      if (!filterL02Selected) setFilterL02Selected(object.DEO);
       setFilterL03Selected(object.CONG_DIST);
       setFilterL04Selected(null);
 
@@ -230,7 +318,7 @@ export default function FilterComponent () {
     if (type === 4) {
       clear_filter(4);
 
-      query_features(4, object);
+      query_features(4, string);
     }
 
     setDataSelected(null);
@@ -301,7 +389,7 @@ export default function FilterComponent () {
       <div id = "filter-container">
         <div className = { dropdown01Active ? "filter-menu-dropdown-active" : "filter-menu-dropdown-inactive" } onClick = { function () { click_dropdown(1); } }>
           <div>
-            <div>{ filterL01Selected ? array_level_01.find(function (object) { return (object.REGION === filterL01Selected); }).L01_NAME : "Region" }</div>
+            <div>{ filterL01Selected || "Region" }</div>
             <div>
               <span className = "material-symbols-outlined">{ dropdown01Active ? "arrow_drop_up" : "arrow_drop_down" }</span>
             </div>
@@ -309,20 +397,30 @@ export default function FilterComponent () {
           <div>
             <div onClick = { function () { clear_filter(1); } }>{ "Clear Selection" }</div>
             {
-              array_level_01 ?
-                array_level_01
+              filterArray && filterArray.length > 0 ?
+                [...new Set(filterArray.map(function (item) { return (item.REGION); }))]
                   .sort(function (base, next) {
-                    if (base.L01_ID && next.L01_ID) {
-                      return (base.L01_ID.localeCompare(next.L01_ID));
+                    if (base && next) {
+                      const base_split = base.split(/[\s-]+/);
+                      base_split[1] = parseRomanToInteger(base_split[1]);
+                      const next_split = next.split(/[\s-]+/);
+                      next_split[1] = parseRomanToInteger(next_split[1]);
+
+                      const base_parsed = base_split[0] === "Region" ? base_split.join(" ") : base;
+                      const next_parsed = next_split[0] === "Region" ? next_split.join(" ") : next;
+
+                      return (base_parsed.localeCompare(next_parsed));
                     }
                     else {
                       return (0);
                     }
                   })
                   .map(function (item, index) {
-                    return (
-                      <div key = { index } className = { filterL01Selected && array_level_01.find(function (object) { return (object.REGION === filterL01Selected); }).L01_NAME === item.L01_NAME ? "filter-menu-item-selected" : null } onClick = { function () { select_filter(1, item); } }>{ item.L01_NAME }</div>
-                    );
+                    if (item !== null) {
+                      return (
+                        <div key = { index } className = { filterL01Selected && filterL01Selected === item ? "filter-menu-item-selected" : null } onClick = { function () { select_filter(1, item); } }>{ item }</div>
+                      );
+                    }
                   })
                 :
                 null
@@ -331,7 +429,7 @@ export default function FilterComponent () {
         </div>
         <div className = { dropdown02Active ? "filter-menu-dropdown-active" : "filter-menu-dropdown-inactive" } onClick = { function () { click_dropdown(2); } }>
           <div>
-            <div>{ filterL02Selected ? array_level_02.find(function (object) { return (object.DEO === filterL02Selected); }).L02_NAME : "District Engineering Office" }</div>
+            <div>{ filterL02Selected || "District Engineering Office" }</div>
             <div>
               <span className = "material-symbols-outlined">{ dropdown02Active ? "arrow_drop_up" : "arrow_drop_down" }</span>
             </div>
@@ -339,23 +437,20 @@ export default function FilterComponent () {
           <div>
             <div onClick = { function () { clear_filter(2); } }>{ "Clear Selection" }</div>
             {
-              array_level_02 ?
-                array_level_02
+              filterArray && filterArray.length > 0 ?
+                [...new Set(filterArray.map(function (item) { if (filterL01Selected && filterL01Selected !== item.REGION) { return (null); } else { return (item.DEO); } }))]
                   .sort(function (base, next) {
-                    if (base.L02_ID && next.L02_ID) {
-                      return (base.L02_ID.localeCompare(next.L02_ID));
+                    if (base && next) {
+                      return (base.localeCompare(next));
                     }
                     else {
                       return (0);
                     }
                   })
                   .map(function (item, index) {
-                    if (filterL01Selected && filterL01Selected !== item.REGION) {
-                      return (null);
-                    }
-                    else {  
+                    if (item !== null) {
                       return (
-                        <div key = { index } className = { filterL02Selected && array_level_02.find(function (object) { return (object.DEO === filterL02Selected); }).L02_NAME === item.L02_NAME ? "filter-menu-item-selected" : null } onClick = { function () { select_filter(2, item); } }>{ item.L02_NAME }</div>
+                        <div key = { index } className = { filterL02Selected && filterL02Selected === item ? "filter-menu-item-selected" : null } onClick = { function () { select_filter(2, item); } }>{ item }</div>
                       );
                     }
                   })
@@ -366,7 +461,7 @@ export default function FilterComponent () {
         </div>
         <div className = { dropdown03Active ? "filter-menu-dropdown-active" : "filter-menu-dropdown-inactive" } onClick = { function () { click_dropdown(3); } }>
           <div>
-            <div>{ filterL03Selected ? array_level_03.find(function (object) { return (object.CONG_DIST === filterL03Selected); }).L03_NAME : "Legislative District" }</div>
+            <div>{ filterL03Selected || "Legislative District" }</div>
             <div>
               <span className = "material-symbols-outlined">{ dropdown03Active ? "arrow_drop_up" : "arrow_drop_down" }</span>
             </div>
@@ -374,26 +469,28 @@ export default function FilterComponent () {
           <div>
             <div onClick = { function () { clear_filter(3); } }>{ "Clear Selection" }</div>
             {
-              array_level_03 ?
-                array_level_03
+              filterArray && filterArray.length > 0 ?
+                [...new Set(filterArray.map(function (item) { if (filterL01Selected && filterL01Selected !== item.REGION) { return (null); } else if (filterL02Selected && filterL02Selected !== item.DEO) { return (null); } else { return (item.CONG_DIST); } }))]
                   .sort(function (base, next) {
-                    if (base.L03_ID && next.L03_ID) {
-                      return (base.L03_ID.localeCompare(next.L03_ID));
+                    if (base && next) {
+                      const base_string_array = /^(.*) ?\((.*)\)$/.exec(base);
+                      const base_order_string = /^(.*) DISTRICT$/.exec(base_string_array[2]);
+                      const base_parsed = base_string_array[1] + " (" + parseOrdinalStringToNumericalString(base_order_string[1]) + " DISTRICT)";
+
+                      const next_string_array = /^(.*) ?\((.*)\)$/.exec(next);
+                      const next_order_string = /^(.*) DISTRICT$/.exec(next_string_array[2]);
+                      const next_parsed = next_string_array[1] + " (" + parseOrdinalStringToNumericalString(next_order_string[1]) + " DISTRICT)";
+
+                      return (base_parsed.localeCompare(next_parsed));
                     }
                     else {
                       return (0);
                     }
                   })
                   .map(function (item, index) {
-                    if (filterL01Selected && filterL01Selected !== item.REGION) {
-                      return (null);
-                    }
-                    else if (filterL02Selected && filterL02Selected !== item.DEO) {
-                      return (null);
-                    }
-                    else {
+                    if (item !== null) {
                       return (
-                        <div key = { index } className = { filterL03Selected && array_level_03.find(function (object) { return (object.CONG_DIST === filterL03Selected); }).L03_NAME === item.L03_NAME ? "filter-menu-item-selected" : null } onClick = { function () { select_filter(3, item); } }>{ item.L03_NAME }</div>
+                        <div key = { index } className = { filterL03Selected && filterL03Selected === item ? "filter-menu-item-selected" : null } onClick = { function () { select_filter(3, item); } }>{ item }</div>
                       );
                     }
                   })
